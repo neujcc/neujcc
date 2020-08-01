082c85b336c5afc01ec07e922646a52bc09c2fe1
ericli0303 <33180705+ericli0303@users.noreply.github.com>
2020-08-01 15:24:53 -0400

    new changes

--- jcc-website/src/App.js
+++ jcc-website/src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import React, {Component} from 'react';
+import Sakura from './Images/sakura_background.jpg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  render() {
+    return (    
+      <body>
+        <div className='background'>
+          <h1 style={{margin: "0em"}}>JCC Website</h1>
+          <p>
+            dl;aksfjdsalkfjhaslfh dafjh;ldjsalkfjds l;dakfjs;lakdfj saf;kj da;lkfjdsal
+            dlaksfjfasfjkadsjadfda
+            dallldkjaf
+          </p>
+          <p>
+            dakfjs
+          </p>
+          <p>
+            damore
+          </p>
+          <h1>
+            more
+          </h1>
+          <h1>
+            more
+          </h1>
+          <h1>
+            more
+          </h1>
+          <h1>
+            more
+          </h1>        <h1>
+            more
+          </h1>
+          <h1>
+            more
+          </h1>        <h1>
+            more
+          </h1>
+          <h1>
+            more
+          </h1>
+        </div>
+
+      </body>
+    )
+  }
 }
 
 export default App;
