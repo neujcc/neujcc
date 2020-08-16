9ce346b21409c7e47bad7f592959a42fd28cb666
ericli0303 <33180705+ericli0303@users.noreply.github.com>
2020-08-16 17:42:22 -0400

    navbar v1

--- jcc-website/src/App.js
+++ jcc-website/src/App.js
@@ -1,24 +1,18 @@
 import './App.css';
 import JCCNavBar from './Components/NavBar/JCCNavBar.js'
 import React, {Component} from 'react';
-<<<<<<< HEAD
 import Sakura from './Images/sakura_background.jpg'
 import Gallery from './Components/Pages/Gallery';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-=======
->>>>>>> 6da9117b6454bb4f61ccc506e57b191642eb4ce1
 
 class App extends Component{
   render() {
     return (    
-<<<<<<< HEAD
       <div>
       <Switch>
         <Route exact path="/Gallery" component={Gallery} />
       </Switch>
       <body>
-=======
->>>>>>> 6da9117b6454bb4f61ccc506e57b191642eb4ce1
         <div className='background'>
           <JCCNavBar />
           <h1 style={{margin: "0em"}}>JCC Website</h1>
@@ -56,12 +50,9 @@ class App extends Component{
             more
           </h1>
         </div>
-<<<<<<< HEAD
 
       </body>
       </div>
-=======
->>>>>>> 6da9117b6454bb4f61ccc506e57b191642eb4ce1
     )
   }
 }
